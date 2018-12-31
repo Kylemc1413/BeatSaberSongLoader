537603d9dce89f3fceda3dac10be2beb33b74ddf
Kyle1413 <kylet1412@gmail.com>
2018-12-31 14:20:16 -0500

    Version

--- SongLoaderPlugin/Plugin.cs
+++ SongLoaderPlugin/Plugin.cs
@@ -7,7 +7,7 @@ namespace SongLoaderPlugin
 {
 	public class Plugin : IPlugin
 	{
-		public const string VersionNumber = "v5.1.2-beta";
+		public const string VersionNumber = "v5.2.0-beta";
 
 		private SceneEvents _sceneEvents;
 		
