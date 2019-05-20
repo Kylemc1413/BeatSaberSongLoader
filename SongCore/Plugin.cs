95ca39d5f18e9bad208d726f753d79877fd3fd7d
Kyle1413 <kylet1412@gmail.com>
2019-05-20 18:57:34 -0400

    Fix exception when generating songcoreextradata file

--- SongCore/Plugin.cs
+++ SongCore/Plugin.cs
@@ -36,12 +36,13 @@ namespace SongCore
             PlatformsInstalled = Utils.IsModInstalled("Custom Platforms"); 
             harmony = HarmonyInstance.Create("com.kyle1413.BeatSaber.SongCore");
             harmony.PatchAll(System.Reflection.Assembly.GetExecutingAssembly());
-            if (!File.Exists(Collections.dataPath)) File.Create(Collections.dataPath);
        //     Collections.LoadExtraSongData();
             UI.BasicUI.GetIcons();
             CustomUI.Utilities.BSEvents.levelSelected += BSEvents_levelSelected;
-           
-            Collections.LoadExtraSongData();
+            if (!File.Exists(Collections.dataPath))
+                File.Create(Collections.dataPath);
+            else
+                Collections.LoadExtraSongData();
 
 
 
